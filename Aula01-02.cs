f98434578b9a3620930d86206ad001ddc5c7ac15
Marcos Marques <marcoshmarques@gmail.com>
2025-09-20 11:58:10 -0300

    Add files via upload

---
+++ Aula01-02.cs
@@ -0,0 +1,34 @@
+﻿// Comentário de uma linha
+
+/* Comentário em bloco  
+ assim eu comento varias linhas.    
+ */
+
+// Declarando variáveis
+
+string nome;
+int anoNascimento;
+int idade;
+
+// Lendo um valor e atribuindo na variável
+Console.WriteLine("Digite um nome: ");
+nome = Console.ReadLine();
+
+
+Console.WriteLine("                                ");
+
+
+Console.WriteLine("Digite o ano de nascimento ");
+// Conversão de tipos
+anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+// Expressão de subtração
+idade = 2025 - anoNascimento;
+
+
+// Escreva na tela uma saudação.
+Console.WriteLine("Seja bem vindo, " + nome); // Pode ser utilizando essa sintáxe ou a da linha de baixo. 
+Console.WriteLine("                                 ");
+Console.WriteLine(nome + " Nasceu em: " + anoNascimento);
+Console.WriteLine("                                 ");
+Console.WriteLine("Então ele tem " + idade + " anos.");
